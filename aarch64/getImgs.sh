6cb9e8d3578b88e5bb30e1d4032215bc3973419a
Ausaci <54447830+Ausaci@users.noreply.github.com>
2021-03-04 09:49:40 +0800

    Update Imgs Links
    Use Ausaci/Auto_Build_OpenWrt

--- aarch64/getImgs.sh
+++ aarch64/getImgs.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
+# Tag API of Armbian_Buster Release
+wget $(curl -s https://api.github.com/repos/Ausaci/Auto_Build_OpenWrt/releases/39173651 | grep browser_download_url | cut -d '"' -f 4)
 
-#sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/mk_openwrt_src_20201028.tar.gz
-#sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/Armbian_20.10_Aml-s9xxx_buster_5.9.2-flippy-47+.img.xz
-#sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/Armbian_20.10_Aml-s9xxx_buster_5.4.73-flippy-47+o.img.xz
-#sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/Armbian_20.10_Beikeyun_buster_5.4.73-flippy-47+o.img.xz
-#sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/Armbian_20.10_L1-Pro_buster_5.4.73-flippy-47+o.img.xz
-
-wget $(curl -s https://api.github.com/repos/HoldOnBro/Actions-OpenWrt/releases/32444440 | grep browser_download_url | cut -d '"' -f 4)
